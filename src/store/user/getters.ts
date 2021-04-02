94998de869d335f77b33274d0ae70980285e1d1f
Louis Chen <scu.louis@gmail.com>
2021-04-02 17:20:18 +0800

    add comment

--- src/store/user/getters.ts
+++ src/store/user/getters.ts
@@ -2,6 +2,7 @@ import { GetterTree } from "vuex";
 import { RootState } from "../types";
 import { UserState } from "./type";
 
+//取得使用者相關資料
 export const getters:GetterTree<UserState,RootState> = {
     getInfo(state):string{
         return `${state.userName} - ${state.email}`;
--- src/store/user/mutations.ts
+++ src/store/user/mutations.ts
@@ -1,10 +1,14 @@
 import { MutationTree } from "vuex";
 import { UserState } from "./type";
 
+/*
+ 設定使用者名稱
+*/
 export enum UserMutations{
     SET_USERNAME = "SET_USERNAME"
 }
 
+//TODO 定義mutation
 export const mutations:MutationTree<UserState> = {
     [UserMutations.SET_USERNAME](state,payLoad){
         state.userName = payLoad;
